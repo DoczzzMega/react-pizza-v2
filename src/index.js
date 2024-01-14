e2db3a0b6a111fadfacde1e8c354e90f6ef6f29c
DoczzzMega <roma.gunko87@gmail.com>
2024-01-14 05:58:16 +0500

    replace BrowserRouter for HashRouter

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom/client';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <BrowserRouter basename={process.env.PUBLIC_URL}>
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Р>
 );
 
 
